38e25f970ee0dce083fcdcc7692380e0d1322de9
Lebogang-900 <164841096+Lebogang-900@users.noreply.github.com>
2025-09-05 21:37:47 +0200

    Create Program.cs

---
+++ Program.cs
@@ -0,0 +1,29 @@
+using AcademicSystem.Data;
+using Csla.Configuration;
+using MudBlazor.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddRazorPages();
+builder.Services.AddServerSideBlazor();
+builder.Services.AddMudServices();
+
+// CSLA
+builder.Services.AddCsla(o => o.AddAspNetCore());
+
+builder.Services.AddSingleton<StudentService>();
+
+var app = builder.Build();
+
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Error");
+}
+
+app.UseStaticFiles();
+app.UseRouting();
+
+app.MapBlazorHub();
+app.MapFallbackToPage("/_Host");
+
+app.Run();
